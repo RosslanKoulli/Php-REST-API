9167b864c9854ae41fb09d0bb590166b5ef5f698
Rosslan Koulli <116555545+RosslanKoulli@users.noreply.github.com>
2025-05-08 15:42:20 +0100

    Get and Post requests added

---
+++ starterFile.php
@@ -0,0 +1,253 @@
+<?php
+    /**
+     * REST api
+     *
+     * This is a REST API for Web development. It handles GET, POST
+     * , PUT, and DELETE requests to perform CRUD(Create, Read, Update,
+     * Delete) operations.
+     * @ Author Rosslan Koulli
+     * @version 2.3
+    **/
+
+    // Set apporaitae header ofr the API's repsonses
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: GET, POST, PUT, DELETE');
+    header('Access-Control-Allow-Methods: Content-Type, Authorization');
+
+    // Handle preflight options requests
+    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+
+    /**
+     *Class PeopleAPI
+     *Handles CRUD Operations for the people table
+     */
+    class PeopleAPI {
+        // DB connections
+        private $db = null;
+
+        // HTTP status code for the response
+        private $statusCode = 200;
+
+        //Repsonse array
+
+        private $result = [];
+
+        /**
+         *Constructor - establishes database connection
+         */
+
+        public function __construct() {
+            try {
+                // Initlialize db connection using PDO
+                $host = 'localhost';
+                $dbname = 'phpAPIdatabase';
+                $username = 'rk738';
+                $password = 'Bagmam4p';
+
+                $this-> db = new PDO("mysql:host=$host;dbname=$dbname; charset=utf8", $username, $password);
+
+                // Set PDO to throw exceptions if an error occurs
+                $this-> db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Set default fetch mode to associative array
+                $this->db ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            } catch (PDOException $e) {
+                // Log dtabase connection error
+                $this -> statusCode = 500;
+                $this -> result = [
+                    'error' => true,
+                    'message' => 'Database connection Error',
+                    'details' => $e->getMessage()
+
+                ];
+
+            }
+        }
+        /**
+         * Destructor - closes the database connections
+        */
+        public function __destruct() {
+            //Close the database connection
+            $this->db = null;
+        }
+
+        /**
+         * Handle API requests
+         *
+         * Finds out which is the appropriate HTTP request
+         * method and calls the necessary method to handle
+         * the request
+        */
+        public function handleRequest() {
+            try {
+                // Stop processing if database connection failed
+                if ($this-> statusCode === 500) {
+                    $this-> sendResponse();
+                    return;
+                }
+
+                // Get request method
+                $method = $_SERVER['REQUEST_METHOD'];
+
+                // Determines the request ID if it's specified
+                $id = null;
+                if (isset($_GET['id'])) {
+                    $id = $this->sanitizeInput($_GET['id']);
+                    if(!is_numeric($id)) {
+                        $this -> statusCode = 400;
+                        $this -> result = [
+                            'error' => true,
+                            'message' => 'Invalid ID Format'
+                        ];
+                        $this-> sendResponse();
+                        return;
+                    }
+                }
+                switch ($method) {
+                    case 'GET':
+                        $this->handleGet($id);
+                        break;
+                    case 'POST':
+                        $this->handlePost();
+                        break;
+                    case 'PUT':
+                        $this->handlePut($id);
+                        break;
+                    case 'DELETE':
+                        $this-> handleDelete($id);
+                        break;
+                    default:
+                        // Method not allowed
+                        $this -> statusCode =  405;
+                        $this -> result = [
+                            'error' => true,
+                            'message' => 'Method not allowed'
+                        ];
+                        break;
+                }
+            } catch (Exception $e) {
+                // Catch any exceptions
+                $this -> statusCode = 500;
+                $this -> result = [
+                    'error' => true,
+                    'message' => 'Internal Server Error',
+                    'details' => $e->getMessage()
+
+                ];
+            }
+            // Send response
+            $this -> sendResponse();
+        }
+        /**
+         * Handling of GET requests
+         *
+         * @param int|null $id Optional ID of the person to retrieve
+        */
+        private function handleGet($id = null) {
+            try {
+                if($id === null){
+
+                    // Retrieve all the people
+                    $stmt = $this->db -> prepare("SELECT * FROM people");
+                    $stmt -> execute();
+                    $people = $stmt->fetchAll();
+
+                    if(count($people)>0) {
+                        $this-> statusCode = 200;
+                        $this->result = $people;
+                    } else {
+                        $this-> statusCode = 204; // No content
+                        $this-> result = [
+                            'message' => 'No people found'
+                        ];
+                    }
+                } else {
+                    // Retrieve specific person by ID
+                    $stmt = $this->db -> prepare("SELECT * FROM people WHERE id = :id");
+                    $stmt -> bindParam(':id', $id, PDO::PARAM_INT);
+                    $stmt -> execute();
+                    $person = $stmt->fetchAll();
+
+                    if($person) {
+                        $this->statusCode = 200;
+                        $this->result = $person;
+                    } else {
+                        $this->statusCode = 404;
+                        $this->result = [
+                            'error' => true,
+                            'message' => 'Person not found'
+                        ];
+                    }
+                }
+            } catch (PDOException $e) {
+                $this -> statusCode = 500;
+                $this -> result = [
+                    'error' => true,
+                    'message' => 'DB Error',
+                    'details' => $e->getMessage()
+                ];
+            }
+        }
+        /**
+         * Handles POST requests (create)
+        */
+        private function handlePost() {
+            // Get input data
+            $inputData = $this->getInputData();
+
+            // Validating required fields
+            if(!$this -> valudatePersonData($inputData)){
+                $this -> statusCode = 400;
+                $this -> result = [
+                    'error' => true,
+                    'message' => 'Missing required fields(firstname, lastname, phone)'
+                ];
+                return;
+            }
+            try {
+                // Inset new person
+                $stmt = $this->db->prepare('
+                INSERT INTO people (firstname, lastname, phone)
+                VALUES (:firstname, :lastname, :phone)
+                ');
+                $stmt->bindParam(':firstname', $inputData['firstname'], PDO::PARAM_STR);
+                $stmt->bindParam(':lastname', $inputData['lastname'], PDO::PARAM_STR);
+                $stmt->bindParam(':phone', $inputData['phone'], PDO::PARAM_STR);
+
+                if($stmt->execute()) {
+                    $id = $this->db->lastInsertId();
+                    $this-> statusCode = 201; // Created
+
+                    // Set location header for newly created resource
+                    header("Location". $this->getBaseUrl(). "?id=$id");
+
+                    $this->result = [
+                        'id' => $id,
+                        'message' =>  'Succesfully created person entry'
+                    ];
+                } else {
+                    $this->statusCode = 500;
+                    $this->result = [
+                        'error' => true,
+                        'message' => 'Failed to create person entry'
+                    ];
+                }
+            } catch (PDOException $e) {
+                $this -> statusCode = 500;
+                $this -> result = [
+                    'error' => true,
+                    'message' => 'Database Error',
+                    'details' => $e->getMessage()
+                ];
+            }
+        }
+        /**
+         * Handle PUT requests (Update)
+        */
+    }
+?>
